a42ff838cf4e439b4b71c8d5d9fbff62ec9e9cfd
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-07-12 21:50:00 +0100

    add plane take off and confirm

--- lib/airport.rb
+++ lib/airport.rb
@@ -5,5 +5,7 @@ class Airport
   def land(plane)
     plane
   end
-  
+
+  def take_off(plane)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -3,14 +3,17 @@ require 'airport'
 describe Airport do
   describe '#land' do
 
-    it 'lands a plane' do
-      expect(subject).to respond_to(:land).with(1).argument
-    end
-
     it 'confirms a plane has landed' do
       plane = Plane.new
       expect(subject.land(plane)).to eq plane
     end
-    
+  end
+
+
+  describe '#take_off' do
+    it 'allows a plane to take off' do
+      plane = Plane.new
+      expect(subject.take_off(plane)).to eq nil
+    end
   end
 end
